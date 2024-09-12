fcddbf4bfdf90ec6d6a199f488434e5533c879c2
trinityzhub <trinity0091+github@gmail.com>
2024-09-13 05:01:21 +0530

    TrinityzHub

--- components/misc/footer.tsx
+++ components/misc/footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="container mx-auto px-5">
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            <PreviewLink href="/statically-generated">Statically Generated</PreviewLink> with Next.js.
+            <PreviewLink href="/statically-generated">Please Checkout </PreviewLink> TrinityzHub.
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
@@ -16,7 +16,7 @@ const Footer = () => {
               Read Documentation
             </a>
             <a
-              href={`https://github.com/matthewwong525/linked-blog-starter`}
+              href={`https://github.com/trinityzhub`}
               className="mx-3 font-bold hover:underline"
             >
               View on GitHub
